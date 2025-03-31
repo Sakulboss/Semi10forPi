4d2d42a7b556411d76d8d37c2c56c584c82e45ab
Konradcmn <123997118+Darnok007@users.noreply.github.com>
2025-03-31 23:54:52 +0200

    code for continous run

--- transistor.py
+++ transistor.py
@@ -6,5 +6,10 @@ GPIO.setwarnings(False)
 GPIO.setup(24, GPIO.OUT)
 
 print ("fan on")
+GPIO.output(24, GPIO.LOW)
 GPIO.output(17, GPIO.LOW)
+GPIO.output(22, GPIO.LOW)
+GPIO.output(23, GPIO.LOW)
+GPIO.output(27, GPIO.LOW)
+
 #time.sleep(10)
