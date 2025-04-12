7de9a132087528467c83abc43385202ccc4a59d6
Konradcmn <123997118+Darnok007@users.noreply.github.com>
2025-04-12 20:42:22 +0200

    stereo test

--- transistor.py
+++ transistor.py
@@ -2,18 +2,12 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(24, GPIO.OUT)
+#GPIO.setwarnings(False)
 GPIO.setup(17, GPIO.OUT)
-GPIO.setup(22, GPIO.OUT)
-GPIO.setup(23, GPIO.OUT)
 GPIO.setup(27, GPIO.OUT)
-
-print ("fan on")
-GPIO.output(24, GPIO.HIGH)
-GPIO.output(17, GPIO.HIGH)
-GPIO.output(22, GPIO.HIGH)
-GPIO.output(23, GPIO.HIGH)
-GPIO.output(27, GPIO.HIGH)
+GPIO.setup(22, GPIO.OUT)
+GPIO.output(17, GPIO.HIGH) # S0
+GPIO.output(27, GPIO.LOW) # S1
+GPIO.output(22, GPIO.LOW) # S2
 
 #time.sleep(10)
