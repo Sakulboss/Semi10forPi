1c5a64ee16528d765b7981dade57f1d60b4e0050
Konradcmn <123997118+Darnok007@users.noreply.github.com>
2025-04-13 03:07:00 +0200

    code for prototype

--- Prototyp Marco/main.py
+++ Prototyp Marco/main.py
@@ -1,5 +1,6 @@
 GPIO.setmode(GPIO.BCM)
 
-	GPIO.setup(17, GPIO.OUT)
-	GPIO.setup(27, GPIO.OUT)
-	GPIO.setup(22, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+
