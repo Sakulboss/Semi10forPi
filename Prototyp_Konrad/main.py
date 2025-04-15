c818c4747d24aadd8b4372d0b27970bd43da2661
Konradcmn <123997118+Darnok007@users.noreply.github.com>
2025-04-15 17:44:20 +0200

    fixed record.py

--- Prototyp_Konrad/main.py
+++ Prototyp_Konrad/main.py
@@ -1,25 +1,11 @@
 from record import *
 from temp import *
-from pydub import AudioSegment
-import os
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(27, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
-
-def convert_all_wav_to_flac(folder_path):
-    # Durchsuche den angegebenen Ordner nach WAV-Dateien
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".wav"):
-            wav_file_path = os.path.join(folder_path, filename)
-            flac_file_path = os.path.splitext(wav_file_path)[0] + '.flac'
-
-            # Lade die WAV-Datei und exportiere sie als FLAC
-            audio = AudioSegment.from_wav(wav_file_path)
-            audio.export(flac_file_path, format="flac")
-            print(f"Umgewandelt: {wav_file_path} zu {flac_file_path}")
 
 # Beispielaufruf
 folder = "Aufnahmen"  # Ersetze dies durch den Pfad zu deinem Ordner
@@ -34,4 +20,3 @@ while True:
     record(60, 17)  # Aufnahme für 60 Sekunden und speichere die Datei
     read_and_save_dht_sensors(gpio_pins)
     sleep(45)
-    convert_all_wav_to_flac("Aufnahmen")
